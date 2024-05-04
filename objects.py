4c0163acbccbffcf8a1d714cf18f838f1d578076
illusiannotice <10sargsyanmesrop@gmail.com>
2024-05-04 20:52:56 +0400

    first commit

---
+++ objects.py
@@ -0,0 +1,18 @@
+
+
+class Object:
+    def __init__(self, pos, speed):
+	self.sizeX = 2
+	self.sizeY = 2
+	self.pos = pos
+	self.speed = speed
+
+class Square(Object):
+    def __init__(self, speed):
+	
+    
+    
+    
+	
+	
+	
