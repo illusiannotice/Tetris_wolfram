d12db5734b674ba54b5c309548f0f1ed128e8e74
illusiannotice <10sargsyanmesrop@gmail.com>
2024-07-17 09:57:48 +0400

    at least it works

--- front_beta/interface_main.py
+++ front_beta/interface_main.py
@@ -1,36 +1,15 @@
 import flet as ft
 
 
-class Display(ft.Container):
+class Display(ft.Column):
     def __init__(self, matrix):
         super().__init__()
         self.matrix = matrix
         self.height = len(matrix)
         self.width = len(matrix[0])
-        self.cols = [ft.Container(width=30, height=30, bgcolor=ft.colors.BLACK, margin=0, padding=0)] * self.width
-        self.displayMatrix = [ft.Row(controls=self.cols, spacing=0)] * self.height
-        self.content = ft.Container(ft.Column(controls=self.displayMatrix, spacing=0))
-        self.margin = 0
-        self.padding = 0
 
-
-
-
-
-    def setmatrix(self, matrix):
-        self.matrix = matrix
-
-    def updatedisplay(self):
-        for i in range(20):
-            print(i)
-            for j in range(10):
-                print(j)
-                if self.matrix[i][j] == 0:
-                    self.displayMatrix[i].controls[j].bgcolor = ft.colors.BLACK
-
-                else:
-                    self.displayMatrix[i].controls[j].bgcolor = ft.colors.WHITE
-
+        self.controls = [ft.Row(controls= [ft.Container(width=30, height=30, bgcolor=ft.colors.BLACK, margin=0, padding=0)  if matrix[j,i] == 0 else ft.Container(width=30, height=30, bgcolor=ft.colors.WHITE, margin=0, padding=0) for i in  range(self.width)], spacing=0) for j in range(self.height)]
+        self.spacing = 0
 
 def main(page: ft.Page) -> None:
     page.title = "tetris"
@@ -41,20 +20,17 @@ def main(page: ft.Page) -> None:
     page.theme = ft.Theme(font_family="OpenSans")
     page.window_width = 350
     page.window_height = 725
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
     page.appbar = ft.AppBar(bgcolor=ft.colors.GREEN_900, title=ft.Text("Tetris", size=30))
     matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]] * 20
     print(matrix)
-    display = Display(matrix, )
+    display = ft.Container(Display(matrix), margin = 0, padding = 0)
     matrix[1][2] = 1
     page.add(display)
     page.update()
-    display.setmatrix(matrix)
-    display.updatedisplay()
+    display.content = Display(matrix)
     page.add(display)
     page.update()
-
 
 
 if __name__ == "__main__":
